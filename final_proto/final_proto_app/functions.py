289dc04ac7bca27578159699bf945fac7b847986
jason.christopher <jason.christopher@nutrifood.co.id>
2021-05-19 00:22:33 +0700

    start api

--- final_proto/final_proto_app/functions.py
+++ final_proto/final_proto_app/functions.py
@@ -10,6 +10,8 @@ from pytchat import LiveChat, CompatibleProcessor
 from chat_downloader import ChatDownloader
 import time
 import pytchat
+import subprocess
+import shlex
 
 def extract_video_id(url):
     # Examples:
@@ -95,65 +97,69 @@ def send_message(url, message,request):
 def view_message(url,request):
     id = extract_video_id(url)
     print(id)
-    token = SocialToken.objects.get(account__user=request.user, account__provider='google')
-    print(token)
-    # CLIENT_SECRET_FILE = 'client_secret_51870834106-rtq1bi2n4n6cme450auv0iffv9fpokre.apps.googleusercontent.com.json'
-    # flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-    credentials = Credentials(
-        token=token.token,
-        refresh_token=token.token_secret,
-        token_uri='https://oauth2.googleapis.com/token',
-        client_id='375686044917-4ip2r585igrkf6kesp3ggmfd45f53433.apps.googleusercontent.com', # replace with yours 
-        client_secret='HZgBiq-fG0_vrfsxHHNA7Ptu') # replace with yours 
-    print("secret token:"+ " "+credentials.refresh_token)
-    service = build('youtube', 'v3', credentials=credentials)
-    print(service)
+    proc = subprocess.Popen(shlex.split('python3 tester3.py ' + str(url)))
+    stdout = proc.communicate()[0]
+    print("is this weird")
+
+    # token = SocialToken.objects.get(account__user=request.user, account__provider='google')
+    # print(token)
+    # # CLIENT_SECRET_FILE = 'client_secret_51870834106-rtq1bi2n4n6cme450auv0iffv9fpokre.apps.googleusercontent.com.json'
+    # # flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+    # credentials = Credentials(
+    #     token=token.token,
+    #     refresh_token=token.token_secret,
+    #     token_uri='https://oauth2.googleapis.com/token',
+    #     client_id='375686044917-4ip2r585igrkf6kesp3ggmfd45f53433.apps.googleusercontent.com', # replace with yours 
+    #     client_secret='HZgBiq-fG0_vrfsxHHNA7Ptu') # replace with yours 
+    # print("secret token:"+ " "+credentials.refresh_token)
+    # service = build('youtube', 'v3', credentials=credentials)
+    # print(service)
     
 
-    part_string = 'snippet,liveStreamingDetails'
-    video_ids = id
-    #find data using API regarding video
-    print(id)
-    response = service.videos().list(
-        part=part_string,
-        id=video_ids
-    ).execute()
+    # part_string = 'snippet,liveStreamingDetails'
+    # video_ids = id
+    # #find data using API regarding video
+    # print(id)
+    # response = service.videos().list(
+    #     part=part_string,
+    #     id=video_ids
+    # ).execute()
 
-    print(response)
-    #gets active livechat id and channelid
-    channelid = response['items'][0]['snippet']['channelId']
-    # print("this is the channel id: " + response['items'][0]['snippet']['channelId'])
-    # print("you are currently watching " + response['items'][0]['snippet']['title'] + " by: " + response['items'][0]['snippet']['channelTitle'])
-    if response['items'][0]['snippet']['liveBroadcastContent'] == 'none':
-        print("hey this isnt a live stream, checking if this is an archive")
-        chat = ChatDownloader().get_chat(url)       # create a generator
-        for message in chat:
-            print(chat.format(message))    
-            context = {'message':chat.format(message)} 
-    else:
-        livechatid = response['items'][0]['liveStreamingDetails']['activeLiveChatId']
-    print("this is the livechat id: " + response['items'][0]['liveStreamingDetails']['activeLiveChatId'])
-    response1 = service.liveChatMessages().list(
-            liveChatId = livechatid,
-            part = 'snippet'
-        ).execute()
-    print(type(response1))
-    print(type(response1['items']))
+    # print(response)
+    # #gets active livechat id and channelid
+    # channelid = response['items'][0]['snippet']['channelId']
+    # # print("this is the channel id: " + response['items'][0]['snippet']['channelId'])
+    # # print("you are currently watching " + response['items'][0]['snippet']['title'] + " by: " + response['items'][0]['snippet']['channelTitle'])
+    # if response['items'][0]['snippet']['liveBroadcastContent'] == 'none':
+    #     print("hey this isnt a live stream, checking if this is an archive")
+    #     chat = ChatDownloader().get_chat(url)       # create a generator
+    #     for message in chat:
+    #         print(chat.format(message))    
+    #         context = {'message':chat.format(message)} 
+    # else:
+    #     livechatid = response['items'][0]['liveStreamingDetails']['activeLiveChatId']
+    # print("this is the livechat id: " + response['items'][0]['liveStreamingDetails']['activeLiveChatId'])
+    # response1 = service.liveChatMessages().list(
+    #         liveChatId = livechatid,
+    #         part = 'snippet'
+    #     ).execute()
+    # print(type(response1))
+    # print(type(response1['items']))
     
-    context1 = {} 
-    for i in range(len(response1['items'])):
-        message = response1['items'][i]['snippet']['textMessageDetails']['messageText']
-        response2 = service.channels().list(
-            id = response1['items'][i]['snippet']['authorChannelId'],
-            part = 'snippet'
-        ).execute()
-        author = response2['items'][0]['snippet']['title']
-        context1['message_entry '+ str(i)] = { "author" : author, 'message':message}
+    # context1 = {} 
+    # for i in range(len(response1['items'])):
+    #     message = response1['items'][i]['snippet']['textMessageDetails']['messageText']
+    #     response2 = service.channels().list(
+    #         id = response1['items'][i]['snippet']['authorChannelId'],
+    #         part = 'snippet'
+    #     ).execute()
+    #     author = response2['items'][0]['snippet']['title']
+    #     context1['message_entry '+ str(i)] = { "author" : author, 'message':message}
     # chat = ChatDownloader().get_chat(url)       # create a generator
     # for message in chat:
     #     print(chat.format(message))    
     #     context1 = {'message':chat.format(message)} 
-    return context1
+    #  
     #sends out a message to the live chat 
     # response2 = service.liveChatMessages().insert(
     #     part = 'snippet',
--- final_proto/tester3.py
+++ final_proto/tester3.py
@@ -1,6 +1,23 @@
 from chat_downloader import ChatDownloader
+import sys
+import json
+import re
+import emoji
 
-url = 'https://www.youtube.com/watch?v=-F-YIA-lhQ0'
-chat = ChatDownloader().get_chat(url)       # create a generator
-for message in chat:                        # iterate over messages
-    print(chat.format(message))             # print the formatted message
+print(str(sys.argv[1]))
+chat = ChatDownloader().get_chat(str(sys.argv[1])) 
+# for message in chat:
+#     return chat.format(message)
+# print(chat)      # create a generator
+print(type(chat))
+
+def give_emoji_free_text(text):
+    return emoji.get_emoji_regexp().sub(r'', text)
+
+
+for message in chat:
+    chatlist  = { }                      # iterate over messages
+    chatlist['author'] = message['author']['name'].encode("ascii", errors="ignore").decode()
+    chatlist['message'] = message['message'].encode("ascii", errors="ignore").decode()
+    with open('chatlist.txt', 'a+') as file:
+      file.write(json.dumps(chatlist, indent=2))
